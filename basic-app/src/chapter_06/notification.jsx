5fe0cfee3f92a027067128ee420e669d70e7f2bd
or.llon <dhflxhdxhd@gmail.com>
2023-04-17 13:59:11 +0900

    Feat : State Life Cycle 적용

--- basic-app/src/chapter_06/notification.jsx
+++ basic-app/src/chapter_06/notification.jsx
@@ -4,14 +4,13 @@ const styles = {
     wrapper: {
         margin: 8,
         padding: 8,
-        display: 'flex',
-        flexDirection: 'row',
-        border: '1px solid grey',
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid grey",
         borderRadius: 16,
     },
-
     messageText: {
-        color: 'black',
+        color: "black",
         fontSize: 16,
     },
 };
@@ -23,12 +22,24 @@ class Notification extends React.Component {
         this.state = {};
     }
 
-    render(){
+    componentDidMount() {
+        console.log(`${this.props.id} componentDidMount() called.`);
+    }
+
+    componentDidUpdate() {
+        console.log(`${this.props.id} componentDidUpdate() called.`);
+    }
+
+    componentWillUnmount() {
+        console.log(`${this.props.id} componentWillUnmount() called.`);
+    }
+
+    render() {
         return (
             <div style={styles.wrapper}>
                 <span style={styles.messageText}>{this.props.message}</span>
             </div>
-        )
+        );
     }
 }
 
--- basic-app/src/chapter_06/notificationList.jsx
+++ basic-app/src/chapter_06/notificationList.jsx
@@ -3,53 +3,65 @@ import Notification from "./notification";
 
 const reservedNotifications = [
     {
-        message: "오늘의 날씨를 알려드립니다."
+        id: 1,
+        message: "안녕하세요, 오늘 일정을 알려드립니다.",
     },
     {
-        message: "곧 출근할 시간입니다."
+        id: 2,
+        message: "점심식사 시간입니다.",
     },
     {
-        message: "공부를 시작하십시오."
+        id: 3,
+        message: "이제 곧 미팅이 시작됩니다.",
     },
 ];
 
 var timer;
 
 class NotificationList extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        // notificationList : notification component를 목록 형태로 보여주기 위한 component
-        // notification이라는 이름의 빈 배열을 state에 넣음
-        // constructor에서는 앞으로 사용할 데이터를 state에 넣어서 초기화
-        this.state = {notification: [],};
+        this.state = {
+            notifications: [],
+        };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const { notifications } = this.state;
-
         timer = setInterval(() => {
-            if (notifications.length < reservedNotifications.length){
+            if (notifications.length < reservedNotifications.length) {
                 const index = notifications.length;
                 notifications.push(reservedNotifications[index]);
-                // state를 업데이트하기 위해 setState 필수
                 this.setState({
                     notifications: notifications,
                 });
-                
-            }else{
+            } else {
+                this.setState({
+                    notifications: [],
+                });
                 clearInterval(timer);
-            }   
+            }
         }, 1000);
     }
 
-    render(){
+    componentWillUnmount() {
+        if (timer) {
+            clearInterval(timer);
+        }
+    }
+
+    render() {
         return (
             <div>
-
                 {this.state.notifications.map((notification) => {
-                    
-                    return <Notification message={notification.message} />
+                    return (
+                        <Notification
+                            key={notification.id}
+                            id={notification.id}
+                            message={notification.message}
+                        />
+                    );
                 })}
             </div>
         );
