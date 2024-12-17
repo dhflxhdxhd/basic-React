f3616c77cdfdfca48f998d5661fb2dc3140643af
or_hyn <dhflxhdxhd@gmail.com>
2024-12-17 18:54:45 +0900

    feat: event 처리

--- basic02/src/components/Button.jsx
+++ basic02/src/components/Button.jsx
@@ -13,14 +13,29 @@ const Button = ({ backgroundColor = "#222", text = "click", children }) => {
     ...ButtonStyle,
   };
 
+  // 이벤트 객체
+  const onClickButton = (event) => {
+    console.log(event);
+    console.log(text);
+  };
   return (
-    <button style={buttonStyle}>
+    <button
+      style={buttonStyle}
+      // onClick={() => {
+      //   // 이벤트 핸들러 (익명 함수 or 선언식 함수)
+      //   console.log(text);
+      // }}
+
+      // [주의] 함수의 호출결과 전달 X (ex. onClickButton())
+      onClick={onClickButton}
+    >
       {text} - {backgroundColor.toUpperCase()}
       {children}
     </button>
   );
 };
 
+// [업데이트 사항] defaultProps 함수형 지원 안함
 // Button.defaultProps = {
 //   backgroundColor: "#222",
 //   text: "click",
