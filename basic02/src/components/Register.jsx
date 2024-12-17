eed1ffb1fcad813f7b93cb5b533408f7ab2270bd
or_hyn <dhflxhdxhd@gmail.com>
2024-12-17 23:11:46 +0900

    feat: state 및 이벤트핸들러 효율적으로 관리하기

--- basic02/src/components/Register.jsx
+++ basic02/src/components/Register.jsx
@@ -8,31 +8,41 @@ import { useState, useEffect } from "react";
 */
 
 const Register = () => {
-  const [name, setName] = useState("김콜라");
-  const [birth, setBirth] = useState("2024-12-03");
-  const [nationality, setNationality] = useState("kr");
-  const [bio, setBio] = useState("");
+  //   const [name, setName] = useState("김콜라");
+  //   const [birth, setBirth] = useState("2024-12-03");
+  //   const [nationality, setNationality] = useState("kr");
+  //   const [bio, setBio] = useState("");
 
-  const onChangeName = (event) => {
-    console.log(event.target.value);
-    setName(event.target.value);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    birth: "",
+    nationality: "",
+    bio: "",
+  });
+
+  console.log(userInfo);
+
+  // 비슷한 이벤트핸들러를 하나의 통합된 핸들러로 사용
+  const onChange = (event) => {
+    console.log(event);
+    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
   };
 
-  const onClickInput = () => {
-    setName("");
-  };
+  //   const onChangeName = (event) => {
+  //     setUserInfo({ ...userInfo, name: event.target.value });
+  //   };
 
-  const onChangeBirth = (event) => {
-    setBirth(event.target.value);
-  };
+  //   const onChangeBirth = (event) => {
+  //     setUserInfo({ ...userInfo, birth: event.target.value });
+  //   };
 
-  const onChangeNationality = (event) => {
-    setNationality(event.target.value);
-  };
+  //   const onChangeNationality = (event) => {
+  //     setUserInfo({ ...userInfo, nationality: event.target.value });
+  //   };
 
-  const onChangeBio = (event) => {
-    setBio(event.target.value);
-  };
+  //   const onChangeBio = (event) => {
+  //     setUserInfo({ ...userInfo, bio: event.target.value });
+  //   };
 
   //   useEffect(() => {
   //     console.log(birth);
@@ -43,28 +53,40 @@ const Register = () => {
       <div>
         <input
           type="text"
-          value={name}
+          name="name"
+          value={userInfo.name}
           placeholder={"이름"}
-          onChange={onChangeName}
-          onClick={onClickInput}
+          onChange={onChange}
         />
       </div>
       <div>
-        <input type="date" value={birth} onChange={onChangeBirth} />
+        <input
+          type="date"
+          name="birth"
+          value={userInfo.birth}
+          onChange={onChange}
+        />
       </div>
       <div>
         {/* select box는 option 요소의 value 속성을 기반으로 값을 인식.
         즉, value 값을 제어하고 업데이트하는 방식 */}
-        <select defaultValue={nationality} onChange={onChangeNationality}>
+        <select
+          name="nationality"
+          defaultValue={userInfo.nationality}
+          onChange={onChange}
+        >
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="eng">영국</option>
           <option value="ger">독일</option>
         </select>
-        {nationality}
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio}></textarea>
+        <textarea
+          value={userInfo.bio}
+          name="bio"
+          onChange={onChange}
+        ></textarea>
       </div>
     </div>
   );
