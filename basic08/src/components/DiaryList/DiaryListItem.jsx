e57e3391cb082487705d829411b202004a48fb49
or_hyn <dhflxhdxhd@gmail.com>
2024-12-27 22:35:04 +0900

    feat: diary list 정렬 기능 추가 & 라우팅 처리

--- basic08/src/components/DiaryList/DiaryListItem.jsx
+++ basic08/src/components/DiaryList/DiaryListItem.jsx
@@ -1,11 +1,22 @@
 import "./DiaryList.css";
-import getEmotionImage from "../../utils/getEmotionImage";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
+import getEmotionImage from "../../utils/getEmotionImage";
 import { formatDate } from "../../utils/dateFormatter";
 const DiaryListItem = ({ diary }) => {
-  const emotionId = 1;
+  const nav = useNavigate();
+
+  const onClickDiaryItem = () => {
+    nav(`diary/${diary.id}`);
+  };
+
+  const onClickEditBtn = (event) => {
+    event.stopPropagation();
+    nav(`/edit/${diary.id}`);
+  };
+
   return (
-    <div className="diary-item" id={diary.id}>
+    <div className="diary-item" id={diary.id} onClick={onClickDiaryItem}>
       <div className={`diary-item__img diary-item__img${diary.emotionId}`}>
         <img src={getEmotionImage(diary.emotionId)} alt="emotion" />
       </div>
@@ -13,7 +24,7 @@ const DiaryListItem = ({ diary }) => {
         <div>{formatDate(diary.createDate)}</div>
         <div>{diary.content}</div>
       </div>
-      <Button text="수정하기" />
+      <Button text="수정하기" onClickButton={onClickEditBtn} />
     </div>
   );
 };
